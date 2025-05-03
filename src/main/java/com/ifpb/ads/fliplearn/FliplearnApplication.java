02709d9fd0039c6937ad96ca5fe75aa28d47c79b
Josias <josiasjt3@gmail.com>
2025-05-03 20:11:32 -0300

    Inicializando projeto.

---
+++ src/main/java/com/ifpb/ads/fliplearn/FliplearnApplication.java
@@ -0,0 +1,13 @@
+package com.ifpb.ads.fliplearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FliplearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FliplearnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ifpb/ads/fliplearn/FliplearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ifpb.ads.fliplearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FliplearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
